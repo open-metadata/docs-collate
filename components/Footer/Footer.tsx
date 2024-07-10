e726d15a97b30b547b364091744ce2b1115e9362
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2024-07-10 17:11:14 +0530

    update footer tabs path

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,19 +1,13 @@
+import { footerItemsData } from "../../constants/Footer.constants";
 import styles from "./Footer.module.css";
-import { footerItemsData } from "../../docs-v1/constants/footer.constants";
 import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className={styles.Container}>
       <div className={styles.InnerContainer}>
-        {footerItemsData.slice(0, 2).map((item) => (
-          <Link
-            className={styles.Link}
-            href={item.href}
-            key={item.href}
-            target={item.target}
-            rel={item.rel}
-          >
+        {footerItemsData.map((item) => (
+          <Link className={styles.Link} href={item.href} key={item.href}>
             {item.name}
           </Link>
         ))}
