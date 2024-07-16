7b27d59d830584c37ed1e0df7b01297afa8fc33f
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2024-07-16 12:50:24 +0530

    updated footer border top

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,10 +1,14 @@
+import { useRouter } from "next/router";
 import { footerItemsData } from "../../constants/Footer.constants";
 import styles from "./Footer.module.css";
 import Link from "next/link";
+import classNames from "classnames";
 
 export default function Footer() {
+  const { pathname } = useRouter();
+
   return (
-    <footer className={styles.Container}>
+    <footer className={classNames(styles.Container, pathname !== "/[version]" && styles.BorderContainer)}>
       <div className={styles.InnerContainer}>
         {footerItemsData.map((item) => (
           <Link className={styles.Link} href={item.href} key={item.href}>
