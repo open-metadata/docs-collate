0a3bb07ba45cb607b4c46bc5411b86534981c1e5
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2024-07-17 15:05:39 +0530

    update image format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,8 +27,8 @@ export default function MyApp({ Component, pageProps }: AppProps<MyAppProps>) {
       <Head>
         <title>{TITLE}</title>
         <link rel="icon" href="/favicon.png" />
-        <link rel="alternate icon" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="alternate icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" />
         <meta name="theme-color" content="#ffffff" />
         <meta content="Collate Docs" property="og:title" />
