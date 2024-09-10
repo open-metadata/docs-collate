fb2529c3f6a208d9643f9c8b25b5a81477d2ee91
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2024-09-10 11:05:02 +0530

    fix(ui): add missing icons (#13)

--- constants/Homepage.constants.ts
+++ constants/Homepage.constants.ts
@@ -1,4 +1,10 @@
 import { HOME_PAGE_BANNER_INFO } from "../docs-v1/constants/homePage.constants";
+import { ReactComponent as DiscoveryIcon } from "../images/header-tabs/discovery.svg";
+import { ReactComponent as LineageIcon } from "../images/header-tabs/lineage.svg";
+import { ReactComponent as ObservabilityIcon } from "../images/header-tabs/observability.svg";
+import { ReactComponent as QualityIcon } from "../images/header-tabs/quality.svg";
+import { ReactComponent as CollaborationIcon } from "../images/header-tabs/collaboration.svg";
+import { ReactComponent as GovernanceIcon } from "../images/header-tabs/governance.svg";
 
 export const COLLATE_HOME_PAGE_BANNER_INFO = {
   ...HOME_PAGE_BANNER_INFO,
@@ -7,38 +13,39 @@ export const COLLATE_HOME_PAGE_BANNER_INFO = {
 
 export const HEADER_INFO = {
   header: "How-to Guides",
-  description: 'Get a complete overview of the features in OpenMetadata from our How-to Guides. The How-to Guides will give you a walk through on accomplishing the basic to the most advanced things in OpenMetadata. These step-by-step guides will help get an overview of the features and also help explore the various functionalities.'
-}
+  description:
+    "Get a complete overview of the features in OpenMetadata from our How-to Guides. The How-to Guides will give you a walk through on accomplishing the basic to the most advanced things in OpenMetadata. These step-by-step guides will help get an overview of the features and also help explore the various functionalities.",
+};
 
 export const HEADER_TABS = [
   {
-    icon: '/images/header-tabs/discovery.svg',
-    name: 'Discovery',
-    link: '/how-to-guides/data-discovery'
+    Icon: DiscoveryIcon,
+    name: "Discovery",
+    link: "/how-to-guides/data-discovery",
   },
   {
-    icon: '/images/header-tabs/lineage.svg',
-    name: 'Lineage',
-    link: '/how-to-guides/data-lineage'
+    Icon: LineageIcon,
+    name: "Lineage",
+    link: "/how-to-guides/data-lineage",
   },
   {
-    icon: '/images/header-tabs/observability.svg',
-    name: 'Observability',
-    link: '/quality-and-observability'
+    Icon: ObservabilityIcon,
+    name: "Observability",
+    link: "/quality-and-observability",
   },
   {
-    icon: '/images/header-tabs/quality.svg',
-    name: 'Quality',
-    link: '/how-to-guides/data-quality-profiler'
+    Icon: QualityIcon,
+    name: "Quality",
+    link: "/how-to-guides/data-quality-profiler",
   },
   {
-    icon: '/images/header-tabs/collaboration.svg',
-    name: 'Collaboration',
-    link: '/how-to-guides/data-collaboration'
+    Icon: CollaborationIcon,
+    name: "Collaboration",
+    link: "/how-to-guides/data-collaboration",
   },
   {
-    icon: '/images/header-tabs/governance.svg',
-    name: 'Governance',
-    link: '/how-to-guides/data-governance'
+    Icon: GovernanceIcon,
+    name: "Governance",
+    link: "/how-to-guides/data-governance",
   },
-]
+];
