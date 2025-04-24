0a3397caa2fb8196de988102eaedfadaecf4c213
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2025-04-24 11:30:13 +0530

    update gtm to remove ga script

--- components/GoogleTagManagerScript/GoogleTagManagerScript.tsx
+++ components/GoogleTagManagerScript/GoogleTagManagerScript.tsx
@@ -2,18 +2,6 @@ import Script from "next/script";
 
 function GoogleTagManagerScript() {
   return (
-    <>
-      {/* Google tag (gtag.js) */}
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-8N4ZPTEDL2" />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-8N4ZPTEDL2');
-        `}
-      </Script>
       <Script
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -23,7 +11,6 @@ function GoogleTagManagerScript() {
             })(window,document,'script','dataLayer','GTM-554C968W');`,
         }}
       />
-    </>
   );
 }
 
